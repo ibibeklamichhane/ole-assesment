deaa6671167d997835e7e77b1591235ecb75e6b7
ibibeklamichhane <bibeklc714@gmail.com>
2025-07-16 23:59:12 +0545

    fix: build fail issue

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import image from '../../public/heroimage.png';
 
 const HeroSection = () => {
--- src/components/services.tsx
+++ src/components/services.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 
 const ServicesSection = () => {
   const services = [
